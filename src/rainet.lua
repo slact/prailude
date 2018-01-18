964013db8b5d8ba711ace2f69831ea799c88d8d7
Leo P <junk@slact.net>
2018-01-18 01:19:11 -0500

    smart frontier puller (presently not as smart as it could be)

--- src/rainet.lua
+++ src/rainet.lua
@@ -1,14 +1,14 @@
 local Peer = require "prailude.peer"
-local logger = require "prailude.log"
+local log = require "prailude.log"
 local bus = require "prailude.bus"
 local config = require "prailude.config"
 local Message = require "prailude.message"
 local Block = require "prailude.block"
 local Timer = require "prailude.util.timer"
 local DB = require "prailude.db"
+local Frontier = require "prailude.frontier"
 
 local uv = require "luv"
-
 local mm = require "mm"
 
 local Rainet = {}
@@ -46,7 +46,7 @@ local function keepalive()
   --keepalive parsing and response
   bus.sub("message:receive:keepalive", function(ok, msg, peer)
     if not ok then
-      return logger:warning("rainet: message:receive:keepalive failed from peer %s: %s", peer, msg)
+      return log:warning("rainet: message:receive:keepalive failed from peer %s: %s", peer, msg)
     end
     peer:update_timestamp("keepalive_received")
     local inpeer
@@ -77,28 +77,28 @@ local function handle_blocks()
   local function check_block(block, peer)
     local ok, err = block:verify()
     if not ok then
-      logger:debug("server: got block that failed verification (%s) from %s", err, tostring(peer))
+      log:debug("server: got block that failed verification (%s) from %s", err, tostring(peer))
     else
       return true
     end
   end
   bus.sub("message:receive:publish", function(ok, msg, peer)
     if not ok then
-      return logger:warning("rainet: message:receive:publish from %s failed: %s", peer, msg)
+      return log:warning("rainet: message:receive:publish from %s failed: %s", peer, msg)
     end
     local block = Block.new(msg.block_type, msg.block)
     check_block(block, peer)
   end)
   bus.sub("message:receive:confirm_req", function(ok, msg, peer)
     if not ok then
-      return logger:warning("rainet: message:receive:confirm_req from %s failed: %s", peer, msg)
+      return log:warning("rainet: message:receive:confirm_req from %s failed: %s", peer, msg)
     end
     local block = Block.new(msg.block_type, msg.block)
     check_block(block, peer)
   end)
   bus.sub("message:receive:confirm_ack", function(ok, msg, peer)
     if not ok then
-      return logger:warning("rainet: message:receive:confirm_ack from %s failed: %s", peer, msg)
+      return log:warning("rainet: message:receive:confirm_ack from %s failed: %s", peer, msg)
     end
     local block = Block.new(msg.block_type, msg.block)
     check_block(block, peer)
@@ -112,39 +112,53 @@ function Rainet.initialize()
 end
 
 function Rainet.bootstrap()
+  local min_frontiers_per_sec = 100
   local fastpeers = Peer.get_fastest_ping(100)
-  local frontier_req = Message.new("frontier_req")
   
   local function frontier_fetcher()
     for _, peer in ipairs(fastpeers) do
-      local frontiers, ok, err
-      logger:debug("bootstrap: starting frontier pull from %s", peer)
+      local frontiers, err
+      log:debug("bootstrap: starting frontier pull from %s", peer)
       --do we have a tcp connection to the peer?
-      ok, err = peer:open_tcp()
-      if not ok then
-        logger:debug("bootstrap: can't init tcp connection to %s (%s)", peer, err)
-        break
-      end
       
-      logger:debug("bootstrap: opened tcp connection to %s", peer)
+      local largest_frontier_pull_size = tonumber(DB.kv.get("largest_frontier_pull") or 0)
+      local prev_frontiers_count, intervals_checked, times_below_min_rate = 0, 0, 0
+      local frontier_pull_size_estimated = false
+      frontiers, err = Frontier.fetch(peer, function(frontiers_so_far, progress)
+        
+        --interval-checker for frontier fetch progress
+        local frontiers_per_sec = #frontiers_so_far - prev_frontiers_count
+        prev_frontiers_count = #frontiers_so_far
+        if frontiers_per_sec < min_frontiers_per_sec then
+          --too slow
+          times_below_min_rate = times_below_min_rate + 1
+          if times_below_min_rate > 2 then
+            return false, ("too slow (%i frontiers/sec)"):format(frontiers_per_sec)
+          end
+        elseif progress > 0.2 and not frontier_pull_size_estimated then
+          --is this pull going to be too small (i.e. from an unsynced node)?
+          frontier_pull_size_estimated = true
+          if #frontiers_so_far * 1/progress < 0.8 * largest_frontier_pull_size then
+            --too small
+            return false, ("node seems desynchronized (small estimated frontier %.0f , expected around %.0f)"):format(#frontiers_so_far * 1/progress, largest_frontier_pull_size)
+          end
+        end
+        log:debug("bootstrap: got %5d frontiers (%7d total) [%4.3f%%] from %s", frontiers_per_sec, #frontiers_so_far, (progress or 0) * 100, peer)
+        intervals_checked = intervals_checked + 1
+        return true
+      end)
       
-      ok, err = peer:send_tcp(frontier_req)
-      if not ok then
-        peer:close_tcp()
-        logger:debug("bootstrap: failed to send frontier req to %s (%s)", tostring(peer), err)
-        break
-      end
-      
-      frontiers, err = peer:read_frontiers() -- TODO: set min frontier-reading speed
       if not frontiers then
-        peer:close_tcp()
-        logger:debug("bootstrap: failed to read frontiers list from peer %s (%s)", tostring(peer), err)
-        break
+        log:debug("bootstrap: failed to get frontiers from %s : %s", peer, err)
+      else
+        log:debug("bootstrap: got %i frontiers from %s", #frontiers, peer)
+        if #frontiers > largest_frontier_pull_size then
+          DB.kv.set("largest_frontier_pull", #frontiers)
+        end
       end
-      
-      
     end
   end
+  
   coroutine.wrap(frontier_fetcher)()
 end
 
