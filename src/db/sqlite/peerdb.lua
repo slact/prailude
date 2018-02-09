da4730d1a4ee9bf6bb59b3c66e698065f4ada8b5
Leo P <junk@slact.net>
2018-02-09 01:06:31 -0500

    do peer stuff in memory,  but save it on exit

--- src/db/sqlite/peerdb.lua
+++ src/db/sqlite/peerdb.lua
@@ -1,26 +1,33 @@
 local sqlite3 = require "lsqlite3"
+local log = require "prailude.log"
 local Peer
 
 local schema = [[
-  CREATE TABLE IF NOT EXISTS peers (
+  CREATE %s IF NOT EXISTS %s (
     address                  TEXT,
     port                     INTEGER,
     last_received            REAL,
     last_sent                REAL,
     last_keepalive_sent      REAL,
     last_keepalive_received  REAL,
+    ping                     REAL NOT NULL DEFAULT 100000,
+    tcp_in_use               INTEGER NOT NULL DEFAULT 0,
+    bootstrap_score          REAL NOT NULL DEFAULT 0,
     PRIMARY KEY(address, port)
   ) WITHOUT ROWID;
   CREATE INDEX IF NOT EXISTS peer_last_received_idx           ON peers (last_received);
   CREATE INDEX IF NOT EXISTS peer_last_sent_idx               ON peers (last_sent);
   CREATE INDEX IF NOT EXISTS peer_last_keepalive_sent_idx     ON peers (last_keepalive_sent);
   CREATE INDEX IF NOT EXISTS peer_last_keepalive_received_idx ON peers (last_keepalive_received);
+  CREATE INDEX IF NOT EXISTS peer_ping_idx                    ON peers (ping);
+  CREATE INDEX IF NOT EXISTS peer_tcp_in_use_idx              ON peers (tcp_in_use);
+  CREATE INDEX IF NOT EXISTS peer_bootstrap_score_idx         ON peers (bootstrap_score);
 ]]
 
 local db
 
-local peer_find, peer_store, peer_get8, peer_get8_except, peer_get_active_to_keepalive, peer_get_fastest_ping
-local peer_update_timestamp = {}
+local peer_find, peer_store, peer_get8, peer_get8_except, peer_get_active_to_keepalive, peer_get_fastest_ping, peer_update_keepalive_ping, peer_get_for_bootstrap
+local peer_update_num = {}
 
 local cache = setmetatable({}, {__mode = "kv"})
 
@@ -102,25 +109,51 @@ local PeerDB_meta = {__index = {
     return peers
   end,
   
-  update_timestamp_field = function(self, timestamp_field)
-    local stmt = peer_update_timestamp[timestamp_field]
+  update_num_field = function(self, field)
+    local stmt = peer_update_num[field]
     assert(stmt, "unknown timestamp field")
-    stmt:bind(1, self[timestamp_field])
+    stmt:bind(1, self[field])
     stmt:bind(2, self.address)
     stmt:bind(3, self.port)
     stmt:step()
     stmt:reset()
     return self
-  end
+  end,
+  
+  update_keepalive_ping = function(self, keepalive_received_time)
+    local stmt = peer_update_keepalive_ping
+    if keepalive_received_time then
+      self.last_keepalive_received = keepalive_received_time
+    else
+      keepalive_received_time = self.last_keepalive_received
+    end
+    stmt:bind(1, keepalive_received_time)
+    stmt:bind(2, keepalive_received_time - self.last_keepalive_sent)
+    stmt:bind(3, self.address)
+    stmt:bind(4, self.port)
+    stmt:step()
+    stmt:reset()
+    return self
+  end,
 }}
 
 return {
-  initialize = function(db_ref)
+  initialize = function(shared_db)
     Peer = require "prailude.peer"
-    db = db_ref
-    if db:exec(schema) ~= sqlite3.OK then
-      error(db:errmsg())
+    db = shared_db
+    
+    assert(db:exec(schema:format("TABLE", "stored_peers")) == sqlite3.OK, db:errmsg())
+    assert(db:exec(schema:format("TEMPORARY TABLE", "peers")) == sqlite3.OK, db:errmsg())
+    
+    --copy previously known peers
+    assert(db:exec("INSERT INTO peers SELECT * FROM stored_peers") == sqlite3.OK, db:errmsg())
+    
+    local numpeers
+    for row in db:urows("SELECT count(*) FROM peers;") do
+      numpeers = row
     end
+    log:debug("loaded %i previously seen peers", numpeers)
+    
     
     peer_find = assert(db:prepare("SELECT * FROM peers WHERE address=? AND port=? LIMIT 1"), db:errmsg())
     peer_store = assert(db:prepare("INSERT OR IGNORE INTO peers (address, port, last_received, last_sent, last_keepalive_sent, last_keepalive_received) "..
@@ -130,19 +163,32 @@ return {
     peer_get8_except = assert(db:prepare("SELECT * FROM peers WHERE last_keepalive_received > datetime('now') - ? AND address !=? AND port != ? ORDER BY RANDOM() LIMIT 8"), db:errmsg())
     peer_get8 = assert(db:prepare("SELECT * FROM peers WHERE last_keepalive_received > datetime('now') - ? ORDER BY RANDOM() LIMIT 8"), db:errmsg())
     peer_get_active_to_keepalive = assert(db:prepare("SELECT * FROM peers WHERE last_keepalive_received > ? AND last_keepalive_received < ? ORDER BY RANDOM()"), db:errmsg())
-    for _, v in pairs{"last_received", "last_sent", "last_keepalive_sent", "last_keepalive_received"} do
-      peer_update_timestamp[v] = assert(db:prepare("UPDATE peers SET " .. v.." = ? WHERE address = ? AND port = ?"), db:errmsg())
+    peer_update_keepalive_ping = assert(db:prepare("UPDATE peers SET last_keepalive_received = ?, ping = ? WHERE address = ? AND port = ?"), db:errmsg())
+    peer_get_for_bootstrap = assert(db:prepare("SELECT * FROM peers WHERE tcp_in_use != 1 ORDER BY bootstrap_score DESC, ping ASC"), db:errmsg())
+    
+    for _, v in pairs{"last_received", "last_sent", "last_keepalive_sent", "last_keepalive_received", "bootstrap_score", "tcp_in_use"} do
+      peer_update_num[v] = assert(db:prepare("UPDATE peers SET " .. v.." = ? WHERE address = ? AND port = ?"), db:errmsg())
     end
+    
     setmetatable(Peer, PeerDB_meta)
   end,
   shutdown = function()
+    --save previously known peers
+    assert(db:exec("INSERT OR REPLACE INTO stored_peers SELECT * FROM peers") == sqlite3.OK, db:errmsg())
+    local numpeers
+    for row in db:urows("SELECT count(*) FROM peers;") do
+      numpeers = row
+    end
+    log:debug("stored %i seen peers", numpeers)
     peer_find:finalize()
     peer_store:finalize()
     peer_get_fastest_ping:finalize()
     peer_get8_except:finalize()
     peer_get8:finalize()
     peer_get_active_to_keepalive:finalize()
-    for _, v in pairs(peer_update_timestamp) do
+    peer_update_keepalive_ping:finalize()
+    peer_get_for_bootstrap:finalize()
+    for _, v in pairs(peer_update_num) do
       v:finalize()
     end
   end
