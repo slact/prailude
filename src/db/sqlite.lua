6ec974a231802e09bb7b7c4f96174f05387c6f2a
Leo P <junk@slact.net>
2018-02-09 01:09:00 -0500

    callbacks for DB  shutdown

--- src/db/sqlite.lua
+++ src/db/sqlite.lua
@@ -1,4 +1,5 @@
 local sqlite3 = require "lsqlite3"
+local Bus = require "prailude.bus"
 
 local subdbs = {
   require "prailude.db.sqlite.peer",
@@ -8,13 +9,25 @@ local subdbs = {
 }
 
 local db
-local DB = {
-  initialize = function(opt)
-    opt = opt or {}
-    db = sqlite3.open(opt.db_file or "raiblocks.db")
-    db:exec("PRAGMA synchronous = OFF") --don't really care if the db lags behind on crash
-    for _, subdb in ipairs(subdbs) do
-      subdb.initialize(db)
+local DB = {}
+
+function DB.initialize(opt)
+  opt = opt or {}
+  db = sqlite3.open(opt.db_file or "raiblocks.db")
+  db:exec("PRAGMA synchronous = OFF") --don't really care if the db lags behind on crash
+  db:exec("PRAGMA journal_mode = TRUNCATE")
+  db:exec("PRAGMA temp_store = MEMORY")
+  db:exec("PRAGMA foreign_keys = OFF")
+  for _, subdb in ipairs(subdbs) do
+    subdb.initialize(db)
+  end
+  Bus.sub("shutdown", DB.shutdown)
+end
+  
+function DB.shutdown()
+  for _, subdb in ipairs(subdbs) do
+    if subdb.shutdown then
+      subdb.shutdown()
     end
   end,
   
@@ -23,6 +36,6 @@ local DB = {
       subdb.shutdown()
     end
   end
-}
+end
 
 return DB
