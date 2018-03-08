0e849fb313e76b83ccf36015992c1d961c429480
Leo P <junk@slact.net>
2018-03-08 03:47:30 -0500

    add util.PageQueue - a 3-level (active/idle/stored) paging FIFO queue

--- src/util/util.lua
+++ src/util/util.lua
@@ -90,6 +90,222 @@ do
   end
 end
 
+local PageQueue; do
+
+  -- load_page(queue_id, page_id); BlockWalker.pop_page(self.walk_id, self.page_id)
+  -- store_page(queue_id, page_id, data)
+  -- store_item(item, page_state)
+  -- stored_page_size(queue_id, page_id)
+  -- page_id
+  
+  local Page_meta = {__index = {
+    state = "idle",
+    change_state = function(self, new_state)
+      local state = self.state
+      if state == new_state then
+        return self --nothing to do
+      end
+      
+      if state == "stored" then -- must at least load from storage
+        local data = self.load_page(self.queue_id, self.id)
+        assert(type(data)=="table", "load_page gave a bad result")
+        self.data = data
+      end
+      
+      if new_state == "idle" and state == "stored" then
+        return self -- we're done here
+      elseif new_state == "active" and (state == "idle" or state == "stored") then
+        local store_item, data = self.store_item, self.data
+        for i, item in ipairs(data) do
+          item = store_item(item, "active")
+          assert(item ~= nil, "store_item gave a nil result")
+          rawset(data, i, item)
+        end
+        self.first, self.last = 1, #self.data
+      end
+      
+      if (new_state == "idle" or new_state == "stored") and state == "active" then
+        --idle pages only store compressed data
+        local store_item, data, newdata = self.store_item, self.data, {}
+        for i=self.first, self.last do
+          local item = store_item(data[i], "idle")
+          assert(item ~= nil, "store_item gave a nil result")
+          table.insert(newdata, item)
+        end
+        self.data, self.first, self.last = newdata, nil, nil
+      end
+      if new_state == "stored" then
+        self.store_page(self.queue_id, self.id, self.data)
+        self.data = nil
+      end
+      self.state = new_state
+    end,
+    
+    add = function(self, item)
+      local state = self.state
+      item = self.store_item(item, state)
+      local data = self.data
+      if not data then
+        data = {}
+        self.data = data
+      end
+      if state == "active" then
+        local last = (rawget(self,"last") or 0) + 1
+        rawset(data, last, item)
+        rawset(self, "last", last)
+        if not self.first then
+          self.first = last
+        end
+        return true
+      elseif state == "idle" then
+        table.insert(data, item)
+      else
+        error("can't add block to " .. tostring(state) .. " page")
+      end
+      return true
+    end,
+    
+    next = function(self)
+      assert(self.state == "active")
+      local data = self.data
+      local first, last = self.first, self.last
+      if not data or not first or not last or first > last then
+        return nil, "empty"
+      end
+      local ret = data[first]
+      data[first] = nil
+      self.first = first + 1
+      return ret
+    end,
+    
+    count = function(self)
+      if self.state == "active" then
+        if self.data then
+          return self.last - self.first + 1
+        else
+          return 0
+        end
+      elseif self.state == "idle" then
+        local data = self.data
+        return data and #data or 0
+      else
+        return self.stored_page_size(self.queue_id, self.id)
+      end
+    end,
+  }}
+  
+  local function Page(queue_id, page_id, data_handlers, state)
+    return setmetatable({
+      queue_id = queue_id,
+      id = page_id,
+      
+      store_item = data_handlers.store_item,
+      store_page = data_handlers.store_page,
+      load_page = data_handlers.load_page,
+      
+      state=state
+    }, Page_meta)
+  end
+
+  
+  local PageQueue_meta = {__index = {
+    pagesize = 5000,
+    add = function(self, item)
+      local lastpage = self.last_page
+      if not lastpage then
+        lastpage = self:add_page()
+      end
+      if lastpage:count() < self.pagesize then
+        return lastpage:add(item)
+      else
+        self:add_page()
+        return self:add(item)
+      end
+    end,
+    
+    add_page = function(self)
+      local next_page_id = self.next_page_id or 0
+      self.next_page_id = next_page_id + 1
+      local page
+      if self.first_page == nil then
+        page = Page(self.id, next_page_id, self.data_handlers, "active")
+        self.first_page = page
+      else
+        if self.last_page ~= self.first_page then
+          self.last_page:change_state("stored")
+        end
+        page = assert(Page(self.id, next_page_id, self.data_handlers, "idle"))
+      end
+      table.insert(self.pages, page)
+      self.last_page = page
+      return page
+    end,
+    
+    next_page = function(self)
+      local removed = table.remove(self.pages, 1) -- remove old first active page
+      if self.last_page == removed then
+        self.last_page = nil
+      end
+      local new_first_page = self.pages[1]
+      self.first_page = new_first_page
+      if new_first_page then
+        new_first_page:change_state("active")
+        return new_first_page
+      end
+    end,
+    
+    next = function(self)
+      local firstpage = self.first_page
+      if firstpage == nil then
+        return nil
+      else
+        local ret = firstpage:next()
+        if not ret then
+          self:next_page()
+          return self:next()
+        else
+          return ret
+        end
+      end
+    end,
+    
+    each = function(self)
+      return function()
+        return self:next()
+      end
+    end,
+    
+    count = function(self)
+      local n = 0
+      for _, page in ipairs(self.pages) do
+        n = n + page:count()
+      end
+      return n
+    end
+    
+  }}
+  
+  function PageQueue(opt)
+    local pq = {
+      pages = {},
+      pagesize = opt.pagesize,
+      id = assert(opt.id, "id missing"),
+      data_handlers = {
+        store_item = assert(opt.store_item, "store_item missing"),
+        store_page = assert(opt.store_page, "store_page missing"),
+        load_page =  assert(opt.load_page,  "load_page missing")
+      }
+    }
+    assert(pq.id, "PageQueue ID required")
+    
+    pq = setmetatable(pq, PageQueue_meta)
+    pq:add_page()
+    return pq
+  end
+
+end
+
+util.PageQueue = PageQueue
 
 local MAX_BATCH_SIZE = 64
 local Ed25519Batch = {
