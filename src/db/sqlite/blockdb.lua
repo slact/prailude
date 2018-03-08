a5d79e8ea4e3e8a770c15b7c5fe25dcc6f99afb6
Leo P <junk@slact.net>
2018-03-08 03:52:20 -0500

    update block schema and some SQL

--- src/db/sqlite/blockdb.lua
+++ src/db/sqlite/blockdb.lua
@@ -1,39 +1,38 @@
 local Block
 local sqlite3 = require "lsqlite3"
 local mm = require "mm"
+local Util = require "prailude.util"
 
-local schema = function(tbl_type, tbl_name, skip_aux_indices)
+local schema = function(tbl_type, tbl_name, skip_indices)
   local _, tbl = tbl_name:match("^(.+%.)(.+)")
   if not tbl then tbl = tbl_name end
   local schema = [[
   CREATE ]]..tbl_type..[[ IF NOT EXISTS ]]..tbl_name..[[ (
-    hash                 TEXT,
-    --raw                  TEXT, --don't store the raw block anymore
-    
-    account              TEXT,
+    hash                 BLOB,
+    account              BLOB,
+    signature            BLOB,
     valid                INTEGER, --0: invalid
                                   --1: PoW ok
                                   --2: signature ok
                                   --3: ledger check ok
                                   --4: confirmed
     
-    n                    INTEGER, --block number in account (OPEN = 0)
     type                 TEXT,
-    prev_block           TEXT, --send, receive, change
-    source               TEXT, --open, receive (source block)
-    representative       TEXT, --open, change  (rep account)
-    destination          TEXT, --send (destination account)
+    previous             BLOB, --send, receive, change (previous block)
+    source               BLOB, --open, receive (source block)
+    representative       BLOB, --open, change  (rep account)
+    destination          BLOB, --send (destination account)
     timestamp            INTEGER,
-    balance              TEXT,
+    genesis_distance     INTEGER,
+    balance              BLOB,
 
     PRIMARY KEY(hash)
   ) WITHOUT ROWID;
-
-  CREATE INDEX IF NOT EXISTS ]]..tbl_name..[[_account_idx      ON ]]..tbl..[[ (account);
-  CREATE INDEX IF NOT EXISTS ]]..tbl_name..[[_prev_idx         ON ]]..tbl..[[ (prev_block);
   ]]
-  if not skip_aux_indices then
+  if not skip_indices then
     schema = schema .. [[
+    CREATE INDEX IF NOT EXISTS ]]..tbl_name..[[_account_idx      ON ]]..tbl..[[ (account);
+    CREATE INDEX IF NOT EXISTS ]]..tbl_name..[[_prev_idx         ON ]]..tbl..[[ (previous);
     CREATE INDEX IF NOT EXISTS ]]..tbl_name..[[_valid_idx        ON ]]..tbl..[[ (valid);
     CREATE INDEX IF NOT EXISTS ]]..tbl_name..[[_type_idx         ON ]]..tbl..[[ (type);
     CREATE INDEX IF NOT EXISTS ]]..tbl_name..[[_source_idx       ON ]]..tbl..[[ (source);
@@ -48,26 +47,33 @@ end
 local sql={}
 
 local cache = setmetatable({}, {__mode="v"})
-local bootstrap_cache = setmetatable({}, {__mode="v"})
-local cache_enabled = false
+local cache_enabled = true
 
 local db
+local function valid_code(valid)
+  if not valid then
+    return 0
+  elseif valid == "PoW" then
+    return 1
+  elseif valid == "signature" then
+    return 2
+  elseif valid == "ledger" then
+    return 3
+  elseif valid == "confirmed" then
+    return 4
+  else
+    error("unknown block validation state " .. tostring(valid))
+  end
+end
 
 local BlockDB_meta = {__index = {
-  find = function(hash, opt)
-    local block, stmt
-    if opt=="bootstrap" then
-      if cache_enabled then
-        block = rawget(bootstrap_cache, hash)
-      end
-      stmt = sql.bootstrap_block_get
-    else
-      if cache_enabled then
-        block = rawget(cache, hash)
-      end
-      stmt = sql.block_get
+  find = function(hash)
+    local block, stmt = nil, sql.block_get
+    if cache_enabled then
+      block = rawget(cache, hash)
     end
     if block == nil then
+      print("CACHE: block "  .. Util.bytes_to_hex(hash) .. " not in cache")
       stmt:bind(1, hash)
       block = stmt:nrows()(stmt)
       --TODO: check for sqlite3.BUSY and such responses
@@ -80,17 +86,26 @@ local BlockDB_meta = {__index = {
       end
       return block
     elseif block == false then
+      print("CACHE: block "  .. Util.bytes_to_hex(hash) .. " does not exist (in cache)")
       return nil
     else
+      print("CACHE: block "  .. Util.bytes_to_hex(hash) .. " found")
       return block
     end
   end,
   
-  find_by_account = function(acct, opt)
+  find_by_account = function(acct)
     local blocks = {}
-    local stmt = opt == "bootstrap" and sql.bootstrap_block_get_by_acct or sql.block_get_by_acct
+    local stmt = sql.block_get_by_acct
     stmt:bind(1, acct)
     for _, block in stmt:nrows() do
+      if cache_enabled then
+        local cached_block = rawget(cache, block.hash)
+        if cached_block then
+          print("CACHE: block "  .. Util.bytes_to_hex(block.hash) .. " found")
+          block = cached_block
+        end
+      end
       table.insert(blocks, Block.new(block))
     end
     stmt:reset()
@@ -101,61 +116,86 @@ local BlockDB_meta = {__index = {
     if self.type == "open" then
       self.n = 1
     end
+    local stmt = opt == "bootstrap" and sql.bootstrap_block_set or sql.block_set
+    stmt:bind(1, assert(self.hash, "block hash missing"))
+    stmt:bind(2, assert(self.account, "block account missing")) -- could be nil, and that's okay
+    stmt:bind(3, assert(self.signature, "block signature missing"))
     
-    local stmt = opt == "bootstrap" and sql.bootstrap_block_set  or sql.block_set
-    stmt:bind(1, self.hash)
-    stmt:bind(2, self.account) -- could be nil, and that's okay
+    local valid = assert(self.valid, "block valid state missing")
+    stmt:bind(4, valid_code(valid))
     
-    local valid = self.valid
-    if not valid then
-      stmt:bind(3, 0)
-    elseif valid == "PoW" then
-      stmt:bind(3, 1)
-    elseif valid == "signature" then
-      stmt:bind(3, 2)
-    elseif valid == "ledger" then
-      stmt:bind(3, 3)
-    elseif valid == "confirmed" then
-      stmt:bind(3, 4)
-    else
-      error("unknown block validation state " .. tostring(valid))
-    end
-    
-    stmt:bind(4, self.n)  --ok if nil
-    stmt:bind(5, self.type) --TODO: use typecode for packing efficiency
-    stmt:bind(6, self.prev_block)
+    stmt:bind(5, assert(self.type, "block type missing")) --TODO: use typecode for packing efficiency
+    stmt:bind(6, self.previous)
     stmt:bind(7, self.source)
     stmt:bind(8, self.representative)
     stmt:bind(9, self.destination)
     stmt:bind(10, self.balance and self.balance:pack() or nil)
+    stmt:bind(11, self.timestamp)
+    stmt:bind(12, self.genesis_distance)
     stmt:step()
     --TODO: check for sqlite3.BUSY and such responses
     stmt:reset()
-    if cache_enabled then
+    if cache_enabled and opt ~= "bootstrap" then
       --update cache
-      if opt ==  "bootstrap" then
-        rawset(bootstrap_cache, self.hash, self)
-      else
-        rawset(cache, self.hash, self)
-      end
+      rawset(cache, self.hash, self)
     end
-    
+    return self
   end,
   
-  batch_store = function(batch, opt)
-    local cachestore = opt == "bootstrap" and bootstrap_cache or cache
+  batch_store_bootstrap = function(batch)
     assert(db:exec("BEGIN EXCLUSIVE TRANSACTION") == sqlite3.OK, db:errmsg())
     for _, block in ipairs(batch) do
-      block:store(opt)
-      if cache_enabled then
-        rawset(cachestore, block.hash, block)
-      end
+      block:store("bootstrap")
+    end
+    assert(db:exec("COMMIT TRANSACTION") == sqlite3.OK, db:errmsg())
+  end,
+  
+  batch_update_ledger_validation = function(batch)
+    print("update batch of size " .. #batch)
+    local stmt = sql.block_update_ledger_validation
+    assert(db:exec("BEGIN EXCLUSIVE TRANSACTION") == sqlite3.OK, db:errmsg())
+    for _, block in ipairs(batch) do
+        stmt:bind(1, valid_code(block.valid))
+        stmt:bind(2, block.genesis_distance)
+        stmt:bind(3, block.hash)
+        stmt:step()
+        stmt:reset()
     end
     assert(db:exec("COMMIT TRANSACTION") == sqlite3.OK, db:errmsg())
   end,
   
   clear_bootstrap = function()
-    assert(db:exec("DELETE FROM bootstrap.blocks") == sqlite3.OK, db:errmsg())
+    assert(db:exec("DELETE FROM disktmp.blocks") == sqlite3.OK, db:errmsg())
+  end,
+  
+  import_unverified_bootstrap_blocks = function()
+    return assert(db:exec("INSERT OR REPLACE INTO blocks SELECT * FROM disktmp.blocks") == sqlite3.OK, db:errmsg())
+  end,
+  
+  get_child_hashes = function(block)
+    local hashes = {}
+    local stmt = sql.get_child_hashes
+    stmt:bind(1, block.hash)
+    stmt:bind(2, block.hash)
+    for hash in stmt:urows() do
+      table.insert(hashes, hash)
+    end
+    stmt:reset()
+    return hashes
+  end,
+  
+  count = function()
+    local stmt = sql.blocks_count
+    local n = stmt:urows()(stmt)
+    stmt:reset()
+    return n
+  end,
+  
+  count_bootstrapped = function()
+    local stmt = sql.bootstrapped_blocks_count
+    local n = stmt:urows()(stmt)
+    stmt:reset()
+    return n
   end
 }}
 
@@ -168,19 +208,25 @@ return {
     assert(db:exec(schema("TABLE", "disktmp.blocks", true)) == sqlite3.OK, db:errmsg())
     
     sql.block_get = assert(db:prepare("SELECT * FROM blocks WHERE hash = ?"), db:errmsg())
-    sql.bootstrap_block_get = assert(db:prepare("SELECT * FROM bootstrap.blocks WHERE hash = ?"), db:errmsg())
     
-    sql.block_get_by_acct = assert(db:prepare("SELECT * FROM blocks WHERE account = ? ORDER BY n DESC"), db:errmsg())
-    sql.bootstrap_block_get_by_acct = assert(db:prepare("SELECT * FROM bootstrap.blocks WHERE account = ? ORDER BY n DESC"), db:errmsg())
+    sql.block_get_by_previous = assert(db:prepare("SELECT * FROM blocks WHERE previous = ?"), db:errmsg())
+    sql.block_get_by_source = assert(db:prepare("SELECT * FROM blocks WHERE source = ?"), db:errmsg())
     
     sql.block_set = assert(db:prepare("INSERT OR REPLACE INTO blocks " ..
-         "(hash, account, valid, n, type, prev_block, source, representative, destination, balance) " ..
-      "VALUES(?,       ?,     ?, ?,    ?,          ?,      ?,              ?,           ?,       ?)", db:errmsg()))
+         "(hash, account, signature, valid, type, previous, source, representative, destination, balance, timestamp, genesis_distance) " ..
+      "VALUES(?,       ?,         ?,     ?,    ?,        ?,      ?,              ?,           ?,       ?,         ?,                ?)", db:errmsg()))
     
-    sql.bootstrap_block_set = assert(db:prepare("INSERT OR REPLACE INTO bootstrap.blocks " ..
-         "(hash, account, valid, n, type, prev_block, source, representative, destination, balance) " ..
-      "VALUES(?,       ?,     ?, ?,    ?,          ?,      ?,              ?,           ?,       ?)", db:errmsg()))
-      
+    sql.bootstrap_block_set = assert(db:prepare("INSERT OR REPLACE INTO disktmp.blocks " ..
+         "(hash, account, signature, valid, type, previous, source, representative, destination, balance, timestamp, genesis_distance) " ..
+      "VALUES(?,       ?,         ?,     ?,    ?,        ?,      ?,              ?,           ?,       ?,         ?,                ?)", db:errmsg()))
+    
+    sql.block_update_ledger_validation = assert(db:prepare("UPDATE blocks SET valid = ?, genesis_distance = ? WHERE hash = ?"), db:errmsg())
+    
+    sql.get_child_hashes = assert(db:prepare("SELECT hash FROM blocks WHERE previous = ? OR source = ?"), db:errmsg())
+    
+    sql.blocks_count = assert(db:prepare("SELECT count(*) FROM blocks"), db:errmsg())
+    sql.bootstrapped_blocks_count = assert(db:prepare("SELECT count(*) FROM disktmp.blocks"), db:errmsg())
+    
     setmetatable(Block, BlockDB_meta)
   end,
   
